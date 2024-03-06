252fd6aab4c4260afaa22647cb056c9da5151986
Rebecca Seepaul <150277065+BeccaCS@users.noreply.github.com>
2024-03-06 13:17:35 -0400

    Added GitHub Link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-     <h1> Weather App</h1>
+      <h1> Weather App</h1>
+
+      <footer>
+        This project is created by{" "}
+        <a href="https://rebeccaseepaul.netlify.app/" target="_blank">
+          Rebecca Seepaul{" "}
+        </a>{" "}
+        and is open-sourced on {""}
+        <a href="https://github.com/BeccaCS/react-app-project" target="_blank">
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
-
-
