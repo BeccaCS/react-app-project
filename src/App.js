19e6fbab365cd3bc82548889ecb613d7a2da1a1a
Rebecca Seepaul <150277065+BeccaCS@users.noreply.github.com>
2024-03-06 15:48:27 -0400

    Added bootstrap and footer with link to my external website

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
+      <div className="Container">
+        <h1> Weather App</h1>
 
-      <footer>
-        This project is created by{" "}
-        <a href="https://rebeccaseepaul.netlify.app/" target="_blank">
-          Rebecca Seepaul{" "}
-        </a>{" "}
-        and is open-sourced on {""}
-        <a href="https://github.com/BeccaCS/react-app-project" target="_blank">
-          GitHub
-        </a>
-      </footer>
+        <footer>
+          This project is created by{" "}
+          <a href="https://rebeccaseepaul.netlify.app/" target="_blank">
+            Rebecca Seepaul{" "}
+          </a>{" "}
+          and is open-sourced on {""}
+          <a
+            href="https://github.com/BeccaCS/react-app-project"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
