a4379d8db11801b4d749f330a2f81bb133ab4eef
Rebecca Seepaul <150277065+BeccaCS@users.noreply.github.com>
2024-03-11 17:55:43 -0400

    Added date

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
+
 import axios from "axios";
 import "./Weather.css";
 
@@ -6,13 +8,12 @@ export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
-    // console.log(response.data);
-
+    
     setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      date: "wednesday 07:00",
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png",
       wind: response.data.wind.speed,
@@ -44,7 +45,10 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+           < FormattedDate date= {weatherData.date} />
+            </li>
+
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row my-3">
@@ -53,7 +57,7 @@ export default function Weather(props) {
             <span className="temperature">
               {Math.round(weatherData.temperature)}
             </span>
-            <span className="unit">°C</span>
+            <span className="unit">°C|°F</span>
           </div>
           <div className="col-6">
             <ul>
